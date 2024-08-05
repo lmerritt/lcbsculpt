817c6244e75e4e74fa0891aeec7c416bcabc04f6
Lloyd Merritt <lloyd.merritt@gmail.com>
2024-08-05 09:51:38 -0500

    add sitemap to robot.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.leaguecitybodysculpt.com/',
+  site: 'https://leaguecitybodysculpt.com/',
   integrations: [icon(), sitemap({
     lastmod: new Date(),
   }),
